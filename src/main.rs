3d86224efd69865d574ed8ac2b49f2751eb0b3bb
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-07-29 22:24:32 +0200

    Upgrade to actix-web 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+use actix_http::httpmessage::HttpMessage;
+use actix_service::Service;
 use actix_web::http::{header, StatusCode};
-use actix_web::middleware::{Finished, Middleware, Started};
+use actix_web::web::{self};
 use actix_web::App as ActixApp;
-use actix_web::{middleware, server, HttpRequest, HttpResponse, Responder, Result};
+use actix_web::HttpResponse;
+use actix_web::HttpServer;
 use chrono::prelude::*;
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg};
+use futures::Future;
 use log::info;
-use simplelog::{Config, LevelFilter, TermLogger};
+use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 use std::net::IpAddr;
 
-#[derive(Clone, Debug)]
+#[derive(Debug, Clone)]
 pub struct DummyhttpConfig {
     quiet: bool,
     verbose: bool,
@@ -161,86 +165,89 @@ pub fn parse_args() -> DummyhttpConfig {
     }
 }
 
-fn respond(req: &HttpRequest<DummyhttpConfig>) -> impl Responder {
-    let status_code = StatusCode::from_u16(req.state().code).unwrap();
-    let mut resp = HttpResponse::with_body(status_code, format!("{}\n", req.state().body));
-    resp.headers_mut().extend(req.state().headers.clone());
+fn default_response(data: web::Data<DummyhttpConfig>) -> HttpResponse {
+    let status_code = StatusCode::from_u16(data.code).unwrap();
+    let mut resp = HttpResponse::with_body(status_code, format!("{}\n", data.body).into());
+    *resp.headers_mut() = data.headers.clone();
     resp
 }
 
-fn configure_app(app: ActixApp<DummyhttpConfig>) -> ActixApp<DummyhttpConfig> {
-    if app.state().verbose {
-        app.middleware(VerboseLogger)
-    } else {
-        app.middleware(middleware::Logger::default())
-    }
-}
-
-struct VerboseLogger;
 struct StartTime(DateTime<Local>);
+//     fn call(&mut self, req: ServiceRequest) -> Self::Future {
+//
+//         Box::new(self.service.call(req).and_then(|res| {
+//             Ok(res)
+//         }))
+//     }
+// }
 
-impl<S> Middleware<S> for VerboseLogger {
-    fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
-        req.extensions_mut().insert(StartTime(Local::now()));
-        Ok(Started::Done)
-    }
-
-    fn finish(&self, req: &HttpRequest<S>, _resp: &HttpResponse) -> Finished {
-        let conn_info = req.connection_info();
-        let remote = conn_info.remote().unwrap_or("unknown");
-        let entry_time = if let Some(entry_time) = req.extensions().get::<StartTime>() {
-            entry_time.0.format("[%d/%b/%Y:%H:%M:%S %z]").to_string()
-        } else {
-            "unknown time".to_string()
-        };
-        let method_path_line = if req.query_string().is_empty() {
-            format!("{} {} {:?}", req.method(), req.path(), req.version())
-        } else {
-            format!(
-                "{} {}?{} {:?}",
-                req.method(),
-                req.path(),
-                req.query_string(),
-                req.version()
-            )
-        };
-        let mut incoming_headers = String::new();
-        for (hk, hv) in req.clone().headers() {
-            incoming_headers.push_str(&format!(
-                "> {}: {}\n",
-                hk.as_str(),
-                hv.to_str().unwrap_or("<unprintable>")
-            ));
-        }
-
-        let incoming_info = format!(
-            "> {method_path_line}\n{headers}",
-            method_path_line = method_path_line,
-            headers = incoming_headers
-        );
-
-        info!(
-            "Connection from {remote} at {entry_time}\n{incoming_info}",
-            remote = remote,
-            entry_time = entry_time,
-            incoming_info = incoming_info,
-        );
-        Finished::Done
-    }
-}
-
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let dummyhttp_config = parse_args();
 
     if !dummyhttp_config.quiet {
-        let _ = TermLogger::init(LevelFilter::Info, Config::default());
+        let _ = TermLogger::init(
+            LevelFilter::Info,
+            Config::default(),
+            TerminalMode::default(),
+        );
     }
 
-    let inside_config = dummyhttp_config.clone();
-    let server = server::new(move || {
-        ActixApp::with_state(inside_config.clone())
-            .configure(configure_app)
-            .default_resource(|r| r.f(respond))
+    let dummyhttp_config_cloned = dummyhttp_config.clone();
+    let server = HttpServer::new(move || {
+        ActixApp::new()
+            .data(dummyhttp_config_cloned.clone())
+            .wrap_fn(|req, srv| {
+                req.extensions_mut().insert(StartTime(Local::now()));
+                srv.call(req).map(|res| {
+                    let req_ = res.request();
+                    let app_state: &DummyhttpConfig =
+                        req_.app_data().expect("There should be data here");
+                    if app_state.verbose {
+                        let conn_info = req_.connection_info();
+                        let remote = conn_info.remote().unwrap_or("unknown");
+                        let entry_time =
+                            if let Some(entry_time) = req_.extensions().get::<StartTime>() {
+                                entry_time.0.format("[%d/%b/%Y:%H:%M:%S %z]").to_string()
+                            } else {
+                                "unknown time".to_string()
+                            };
+                        let method_path_line = if req_.query_string().is_empty() {
+                            format!("{} {} {:?}", req_.method(), req_.path(), req_.version())
+                        } else {
+                            format!(
+                                "{} {}?{} {:?}",
+                                req_.method(),
+                                req_.path(),
+                                req_.query_string(),
+                                req_.version()
+                            )
+                        };
+                        let mut incoming_headers = String::new();
+                        for (hk, hv) in req_.headers() {
+                            incoming_headers.push_str(&format!(
+                                "> {}: {}\n",
+                                hk.as_str(),
+                                hv.to_str().unwrap_or("<unprintable>")
+                            ));
+                        }
+
+                        let incoming_info = format!(
+                            "> {method_path_line}\n{headers}",
+                            method_path_line = method_path_line,
+                            headers = incoming_headers
+                        );
+
+                        info!(
+                            "Connection from {remote} at {entry_time}\n{incoming_info}",
+                            remote = remote,
+                            entry_time = entry_time,
+                            incoming_info = incoming_info,
+                        );
+                    }
+                    res
+                })
+            })
+            .default_service(web::route().to(default_response))
     })
     .bind(format!(
         "{}:{}",
@@ -249,5 +256,5 @@ fn main() {
     .expect("Couldn't bind server")
     .shutdown_timeout(0);
 
-    server.run();
+    server.run()
 }
