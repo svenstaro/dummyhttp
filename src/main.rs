c25735abd2a701cd12b8217811867a2a136ca40a
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-08-27 02:22:24 +0200

    Switch to structopt and allow multiple interfaces

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,66 @@
 use actix_http::httpmessage::HttpMessage;
 use actix_service::Service;
+use actix_web::http::header::HeaderMap;
 use actix_web::http::{header, StatusCode};
 use actix_web::web::{self};
 use actix_web::App as ActixApp;
 use actix_web::HttpResponse;
 use actix_web::HttpServer;
 use chrono::prelude::*;
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg};
 use futures::Future;
 use log::info;
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
-use std::net::IpAddr;
+use std::io::{Error, ErrorKind};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use structopt::StructOpt;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, StructOpt)]
+#[structopt(
+    name = "dummyhttp",
+    raw(global_settings = "&[structopt::clap::AppSettings::ColoredHelp]")
+)]
 pub struct DummyhttpConfig {
+    /// Be quiet (log nothing)
+    #[structopt(short, long)]
     quiet: bool,
+
+    /// Be verbose (log data of incoming and outgoing requests)
+    #[structopt(short, long)]
     verbose: bool,
+
+    /// Port on which to listen
+    #[structopt(short, long, default_value = "8080")]
     port: u16,
-    headers: header::HeaderMap,
+
+    /// Headers to send (format: key:value)
+    #[structopt(short, long, parse(try_from_str = "parse_header"))]
+    headers: Vec<HeaderMap>,
+
+    /// HTTP status code to send
+    #[structopt(short, long, default_value = "200")]
     code: u16,
+
+    /// HTTP body to send
+    #[structopt(short, long, default_value = "dummyhttp")]
     body: String,
-    interface: IpAddr,
+
+    /// Interface to bind to
+    #[structopt(
+        short = "i",
+        long = "interfaces",
+        parse(try_from_str = "parse_interface"),
+        raw(number_of_values = "1"),
+        default_value = "0.0.0.0"
+    )]
+    interfaces: Vec<IpAddr>,
 }
 
-fn is_valid_port(port: String) -> Result<(), String> {
-    port.parse::<u16>()
-        .and(Ok(()))
-        .or_else(|e| Err(e.to_string()))
+/// Checks wether an interface is valid, i.e. it can be parsed into an IP address
+fn parse_interface(src: &str) -> Result<IpAddr, std::net::AddrParseError> {
+    src.parse::<IpAddr>()
 }
 
-fn is_valid_status_code(code: String) -> Result<(), String> {
-    StatusCode::from_bytes(code.as_bytes())
-        .and(Ok(()))
-        .or_else(|e| Err(e.to_string()))
-}
-
-fn is_valid_interface(interface: String) -> Result<(), String> {
-    interface
-        .parse::<IpAddr>()
-        .and(Ok(()))
-        .or_else(|e| Err(e.to_string()))
-}
-
-fn is_valid_header(header: String) -> Result<(), String> {
+fn parse_header(header: &str) -> Result<HeaderMap, String> {
     let header: Vec<&str> = header.split(':').collect();
     if header.len() != 2 {
         return Err("Wrong header format".to_string());
@@ -51,131 +69,35 @@ fn is_valid_header(header: String) -> Result<(), String> {
     let (header_name, header_value) = (header[0], header[1]);
 
     let hn = header::HeaderName::from_lowercase(header_name.to_lowercase().as_bytes())
-        .map(|_| ())
-        .map_err(|e| e.to_string());
+        .map_err(|e| e.to_string())?;
 
-    let hv = header::HeaderValue::from_str(header_value)
-        .map(|_| ())
-        .map_err(|e| e.to_string());
+    let hv = header::HeaderValue::from_str(header_value).map_err(|e| e.to_string())?;
 
-    hn.and(hv)
-}
-
-pub fn parse_args() -> DummyhttpConfig {
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .global_setting(AppSettings::ColoredHelp)
-        .arg(
-            Arg::with_name("quiet")
-                .short("q")
-                .long("quiet")
-                .help("Be quiet (log nothing)"),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .short("v")
-                .long("verbose")
-                .conflicts_with("quiet")
-                .help("Be verbose (log everything)"),
-        )
-        .arg(
-            Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .help("Port to use")
-                .validator(is_valid_port)
-                .required(false)
-                .default_value("8080")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("header")
-                .short("H")
-                .long("header")
-                .help("Header to send (format: key:value)")
-                .validator(is_valid_header)
-                .required(false)
-                .multiple(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("code")
-                .short("c")
-                .long("code")
-                .help("HTTP status code to send")
-                .validator(is_valid_status_code)
-                .required(false)
-                .default_value("200")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("body")
-                .short("b")
-                .long("body")
-                .help("HTTP body to send")
-                .required(false)
-                .default_value("dummyhttp")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("interface")
-                .short("i")
-                .long("if")
-                .help("Interface to listen on")
-                .validator(is_valid_interface)
-                .required(false)
-                .default_value("0.0.0.0")
-                .takes_value(true),
-        )
-        .get_matches();
-
-    let quiet = matches.is_present("quiet");
-    let verbose = matches.is_present("verbose");
-    let port = matches.value_of("port").unwrap().parse().unwrap();
-    let headers = if matches.is_present("header") {
-        let headers_raw = matches.values_of("header").unwrap();
-
-        let mut headers = header::HeaderMap::new();
-        for header in headers_raw {
-            let header_parts: Vec<String> = header.split(':').map(|x| x.to_string()).collect();
-            headers.append(
-                header::HeaderName::from_lowercase(header_parts[0].to_lowercase().as_bytes())
-                    .expect("Invalid header name"),
-                header_parts[1].parse().expect("Invalid header value"),
-            );
-        }
-        headers
-    } else {
-        header::HeaderMap::new()
-    };
-    let code = matches.value_of("code").unwrap().parse().unwrap();
-    let body = matches.value_of("body").unwrap().parse().unwrap();
-    let interface = matches.value_of("interface").unwrap().parse().unwrap();
-
-    DummyhttpConfig {
-        quiet,
-        verbose,
-        port,
-        headers,
-        code,
-        body,
-        interface,
-    }
+    let mut map = HeaderMap::new();
+    map.insert(hn, hv);
+    Ok(map)
 }
 
 fn default_response(data: web::Data<DummyhttpConfig>) -> HttpResponse {
     let status_code = StatusCode::from_u16(data.code).unwrap();
     let mut resp = HttpResponse::with_body(status_code, format!("{}\n", data.body).into());
-    *resp.headers_mut() = data.headers.clone();
+
+    let mut headers = header::HeaderMap::new();
+    for header in &data.headers {
+        // There should only be a single Header in each HeaderMap that we parsed from the command
+        // line arguments.
+        let val = header.iter().next().unwrap();
+        headers.insert(val.0.clone(), val.1.clone());
+    }
+
+    *resp.headers_mut() = headers;
     resp
 }
 
 struct StartTime(DateTime<Local>);
 
 fn main() -> Result<(), std::io::Error> {
-    let dummyhttp_config = parse_args();
+    let dummyhttp_config = DummyhttpConfig::from_args();
 
     if !dummyhttp_config.quiet {
         let _ = TermLogger::init(
@@ -184,6 +106,36 @@ fn main() -> Result<(), std::io::Error> {
             TerminalMode::default(),
         );
     }
+
+    let interfaces = dummyhttp_config
+        .interfaces
+        .iter()
+        .map(|&interface| {
+            if interface == IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)) {
+                // If the interface is 0.0.0.0, we'll change it to 127.0.0.1 so that clicking the link will
+                // also work on Windows. Why can't Windows interpret 0.0.0.0?
+                "127.0.0.1".to_string()
+            } else if interface.is_ipv6() {
+                // If the interface is IPv6 then we'll print it with brackets so that it is clickable.
+                format!("[{}]", interface)
+            } else {
+                format!("{}", interface)
+            }
+        })
+        .collect::<Vec<String>>();
+
+    let socket_addresses = interfaces
+        .iter()
+        .map(|interface| {
+            format!(
+                "{interface}:{port}",
+                interface = &interface,
+                port = dummyhttp_config.port,
+            )
+            .parse::<SocketAddr>()
+        })
+        .collect::<Result<Vec<SocketAddr>, _>>()
+        .map_err(|e| Error::new(ErrorKind::Other, e))?;
 
     let dummyhttp_config_cloned = dummyhttp_config.clone();
     let server = HttpServer::new(move || {
@@ -242,10 +194,7 @@ fn main() -> Result<(), std::io::Error> {
             })
             .default_service(web::route().to(default_response))
     })
-    .bind(format!(
-        "{}:{}",
-        &dummyhttp_config.interface, dummyhttp_config.port
-    ))
+    .bind(socket_addresses.as_slice())
     .expect("Couldn't bind server")
     .shutdown_timeout(0);
 
